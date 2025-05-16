09bbd7db8ea72b09e3bf9ae3579c1129fed1da93
Bùi Đa Vít <anfha.vaf@gmail.com>
2025-05-16 09:13:55 +0700

    afpk 2 beta

--- helper/index.mjs
+++ helper/index.mjs
@@ -1,7 +1,17 @@
+import { encode, decode } from './crypt.mjs'
 export * as objHelper from './obj.mjs'
 
 export const arrHelper = {
   check2Array(array1, array2) {
     return array1.some(item => array2.includes(item))
+  }
+}
+
+export default function helper() {
+  this.helper = {
+    crypt: {
+      encode,
+      decode,
+    },
   }
 }
--- index.js
+++ index.js
@@ -5,32 +5,42 @@ export { default as EOn } from './modun/EOn.mjs'
 export { default as Interact } from './modun/Interact.mjs'
 // export { default as Device } from './modun/Device.mjs'
 
-import * as app from './helper/app.mjs'
-import * as crypt from './helper/crypt.mjs'
-import flang from './helper/flang.mjs'
-import flog from './helper/flog.mjs'
-import * as obj from './helper/obj.mjs'
-import * as file from './helper/file.mjs'
-import * as time from './helper/time.mjs'
-import regFn from './helper/regFn.mjs'
+// import * as app from './helper/app.mjs'
+// import * as crypt from './helper/crypt.mjs'
+// import flang from './helper/flang.mjs'
+// import flog from './helper/flog.mjs'
+// import * as obj from './helper/obj.mjs'
+// import * as file from './helper/file.mjs'
+// import * as time from './helper/time.mjs'
+// import regFn from './helper/regFn.mjs'
 
-export * as server from './lib/server.mjs'
+import helper from './helper/index.mjs'
+
+import server from './lib/server.mjs'
+
 export { default as createDb } from './lib/sqlite.mjs'
 // export * as google from './lib/google.mjs'
-export { default as Database } from 'better-sqlite3'
+import Database from 'better-sqlite3'
 
-export const helper = {
-  app,
-  crypt,
-  flang,
-  flog,
-  obj,
-  file,
-  time,
-  regFn,
-}
+// export const helper = {
+//   app,
+//   crypt,
+//   flang,
+//   flog,
+//   obj,
+//   file,
+//   time,
+//   regFn,
+// }
 
 // triển khai phiên bản v2 tự động nhúng
 export default function () {
-  //
+  // thêm các hàm trợ giúp mặc định
+  helper.bind(this)()
+
+  // thêm thư viện sqlite3
+  this.Database = Database
+
+  // thêm thư viện cấu hình máy chủ
+  server.bind(this)()
 }
